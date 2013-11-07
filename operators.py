f10e293a54567b59d23928e16a9cefa325cea169
Chen Lin <chen@scorebeyond.com>
2013-11-07 09:21:47 -0800

    Changed indent to four spaces for PEP8 compatability.

--- operators.py
+++ operators.py
@@ -1,43 +1,50 @@
 from base import BaseOperator, Noncommutative
 
+
 class Brackets(BaseOperator):
-   def __repr__(self):
-      s = "(%s)"
-      return s % super(Brackets, self).__repr__()
+    def __repr__(self):
+        s = "(%s)"
+        return s % super(Brackets, self).__repr__()
+
 
 class Plus(BaseOperator):
-   sign = '+'
+    sign = '+'
+
 
 class Mult(BaseOperator):
-   sign = '*'
-   # TODO: This is kind of hackish and messy: Should probably find a better way to do this (metaclass?)
-   def __new__(cls, *args, **kwargs):
-      if args[0] == -1 and len(args) == 2:
-         n = args[0] * args[1]
-         return n
-      else:
-         return super(Mult, cls).__new__(cls)
+    sign = '*'
+    # TODO: This is kind of hackish and messy: Should probably find a better way to do this (metaclass?)
+    def __new__(cls, *args, **kwargs):
+        if args[0] == -1 and len(args) == 2:
+            n = args[0] * args[1]
+            return n
+        else:
+            return super(Mult, cls).__new__(cls)
 
-   def __init__(self, *args):
-      # assume -1 means that this has been converted from -n to -1 * n.
-      # in this case, we want to change this back for presentation purposes.
-      args = list(args)
-      if args[0] == -1:
-         del args[0]
-         args[0] *= -1
+    def __init__(self, *args):
+        # assume -1 means that this has been converted from -n to -1 * n.
+        # in this case, we want to change this back for presentation purposes.
+        args = list(args)
+        if args[0] == -1:
+            del args[0]
+            args[0] *= -1
 
-      super(Mult, self).__init__(*args)
+        super(Mult, self).__init__(*args)
+
 
 class Pow(Noncommutative):
-   sign = '^'
-   order = Noncommutative.RIGHT_TO_LEFT
+    sign = '^'
+    order = Noncommutative.RIGHT_TO_LEFT
+
 
 class Minus(BaseOperator):
-   sign = '-'
+    sign = '-'
+
 
 class Fraction(Noncommutative):
-   sign = '/'
-   order = Noncommutative.LEFT_TO_RIGHT
+    sign = '/'
+    order = Noncommutative.LEFT_TO_RIGHT
+
 
 class Eq(BaseOperator):
-   sign = '='
+    sign = '='
--- parse.py
+++ parse.py
@@ -4,92 +4,94 @@ from decimal import Decimal as D
 from collections import deque, OrderedDict
 from operators import *
 
+
 class ParseError(Exception):
-   pass
+    pass
+
 
 class EquationParser(object):
-   def parse(self, equation):
-      """
-      Preprocess the equation before feeding it to the parser.
+    def parse(self, equation):
+        """
+        Preprocess the equation before feeding it to the parser.
 
-      1. Removes spaces.
-      2. Changes minus operators into + -n.
-      """
-      return self._parse(deque(equation))
+        1. Removes spaces.
+        2. Changes minus operators into + -n.
+        """
+        return self._parse(deque(equation))
 
-   def _parse(self, equation):
-      """
-      Parses an equation in canonical format into an expression tree.
+    def _parse(self, equation):
+        """
+        Parses an equation in canonical format into an expression tree.
 
-      Don't call directly--let parse() preprocess the equation first.
-      """
-      OPERATOR_TO_CLASS_MAP = OrderedDict([
-         ('^', Pow),
-         ('*', Mult),
-         ('/', Fraction),
-         ('+', Plus),
-      ])
-      ORDER_OF_OPERATIONS = ['^', '*/', '+']
-      OPEN_BRACKETS = "({["
-      CLOSE_BRACKETS = "]})"
+        Don't call directly--let parse() preprocess the equation first.
+        """
+        OPERATOR_TO_CLASS_MAP = OrderedDict([
+            ('^', Pow),
+            ('*', Mult),
+            ('/', Fraction),
+            ('+', Plus),
+        ])
+        ORDER_OF_OPERATIONS = ['^', '*/', '+']
+        OPEN_BRACKETS = "({["
+        CLOSE_BRACKETS = "]})"
 
-      symbols = []
-      operators = []
+        symbols = []
+        operators = []
 
-      is_letter = lambda x: x.lower() in string.lowercase
-      is_number = lambda x: x in '0123456789.'
-      parse_number = lambda x: D(x) if '.' in x else int(x)
+        is_letter = lambda x: x.lower() in string.lowercase
+        is_number = lambda x: x in '0123456789.'
+        parse_number = lambda x: D(x) if '.' in x else int(x)
 
-      def match_sequence(equation, match_fn):
-         o = array('c')
-         while equation:
-            if match_fn(equation[0]):
-               o.append(equation.popleft())
-            else:
-               break
+        def match_sequence(equation, match_fn):
+            o = array('c')
+            while equation:
+                if match_fn(equation[0]):
+                    o.append(equation.popleft())
+                else:
+                    break
 
-         return o.tostring()
+            return o.tostring()
 
-      # parse everything into either a symbol or operator
-      while equation:
-         char = equation.popleft()
+        # parse everything into either a symbol or operator
+        while equation:
+            char = equation.popleft()
 
-         if char in OPERATOR_TO_CLASS_MAP.keys():
-            operators.append(char)
+            if char in OPERATOR_TO_CLASS_MAP.keys():
+                operators.append(char)
 
-         elif char in OPEN_BRACKETS:
-            symbols.append(Brackets(self._parse(equation)))
+            elif char in OPEN_BRACKETS:
+                symbols.append(Brackets(self._parse(equation)))
 
-         elif char in CLOSE_BRACKETS:
-            break
+            elif char in CLOSE_BRACKETS:
+                break
 
-         # must be a negative sign, since subtraction is changed into + -n.
-         elif char == '-':
-            symbols.append(-1)
-            operators.append('*')
+            # must be a negative sign, since subtraction is changed into + -n.
+            elif char == '-':
+                symbols.append(-1)
+                operators.append('*')
 
-         elif is_number(char):
-            n = char + match_sequence(equation, is_number)
-            symbols.append(parse_number(n))
+            elif is_number(char):
+                n = char + match_sequence(equation, is_number)
+                symbols.append(parse_number(n))
 
-         elif is_letter(char):
-            symb = char + match_sequence(equation, is_letter)
-            symbols.append(symb)
+            elif is_letter(char):
+                symb = char + match_sequence(equation, is_letter)
+                symbols.append(symb)
 
-      # parse symbol and operator lists into expression tree, following order of operations
-      for current_op in ORDER_OF_OPERATIONS:
-         index = 0
-         while index < len(operators):
-            op = operators[index]
-            symbs = []
-            if operators[index] in current_op:
-               # this is the beginning of an operator chain (i.e. 4 * x * y * x)
-               while index < len(operators) and operators[index] == op:
-                  symbs.append(symbols.pop(index))
-                  operators.pop(index)
-               symbs.append(symbols.pop(index))
-               symbols.insert(index, OPERATOR_TO_CLASS_MAP[op](*symbs))
-            else:
-               index += 1
+        # parse symbol and operator lists into expression tree, following order of operations
+        for current_op in ORDER_OF_OPERATIONS:
+            index = 0
+            while index < len(operators):
+                op = operators[index]
+                symbs = []
+                if operators[index] in current_op:
+                    # this is the beginning of an operator chain (i.e. 4 * x * y * x)
+                    while index < len(operators) and operators[index] == op:
+                        symbs.append(symbols.pop(index))
+                        operators.pop(index)
+                    symbs.append(symbols.pop(index))
+                    symbols.insert(index, OPERATOR_TO_CLASS_MAP[op](*symbs))
+                else:
+                    index += 1
 
-      return symbols[0]
+        return symbols[0]
