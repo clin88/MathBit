2c9315ce2b9ad5ce76e2b292e9520c8651263122
Chen Lin <chen@scorebeyond.com>
2013-11-07 22:01:42 -0800

    Added order of operations class variables to operators.

--- operators.py
+++ operators.py
@@ -1,32 +1,43 @@
 from base import BaseOperator, Noncommutative
 
+"""
+    Order of operations:
+
+        () = 0
+        ^ = 1
+        */ = 2
+        +- = 3
+"""
+
 
 class Brackets(BaseOperator):
+    oop = '0'
+
     def __repr__(self):
         s = "(%s)"
         return s % super(Brackets, self).__repr__()
 
 
-class Plus(BaseOperator):
-    sign = '+'
+class Pow(Noncommutative):
+    sign = '^'
+    oop = '1'
+    order = Noncommutative.RIGHT_TO_LEFT
 
 
 class Mult(BaseOperator):
     sign = '*'
-
-
-class Minus(BaseOperator):
-    sign = '-'
-
-
-class Pow(Noncommutative):
-    sign = '^'
-    order = Noncommutative.RIGHT_TO_LEFT
+    oop = '2'
 
 
 class Fraction(Noncommutative):
     sign = '/'
+    oop = '2'
     order = Noncommutative.LEFT_TO_RIGHT
+
+
+class Plus(BaseOperator):
+    sign = '+'
+    oop = '3'
 
 
 class Eq(BaseOperator):
