785fe4bc9f1ecfe3c3affc48ffb8688cf6ada1a8
Chen Lin <chen@scorebeyond.com>
2013-11-08 19:37:41 -0800

    Adding function to help walk through expressions.

--- expression_helpers.py
+++ expression_helpers.py
@@ -1,3 +1,20 @@
+from base import BaseOperator
+from operators import *
+
+def walk_expr(expr, *types):
+    """
+        Generator for iterating through all objects of type in *types.
+    """
+    for child in expr.children:
+        # yield this node if no type specified OR this type is in *types
+        if not types or type(child) in types:
+            yield child
+
+        if isinstance(child, BaseOperator):
+            for obj in walk_expr(child, *types):
+                yield obj
+
+
 def mathify(obj):
     if isinstance(obj, D) or isinstance(obj, int) or isinstance(obj, float):
         return Number(obj)
